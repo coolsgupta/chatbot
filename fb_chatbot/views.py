23a45972c16e916ef4015df7a941eb4b2bb67ac7
= <=>
2016-08-07 13:01:44 +0530

    adding a random quote-6

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -66,7 +66,8 @@ def post_facebook_message(fbid, recevied_message):
         joke_text = 'Yo ' + reply_text
 
     random_quote = return_random_quote()
-    joke_text = random_quote[0]
+    #joke_text = random_quote[0]
+    joke_text='fuck off sahil'
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": joke_text}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
